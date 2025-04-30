c638e23e60fbe4be8eb71da9a7d0d8018234227a
Myra <louisnyembo2@gmail.com>
2025-04-30 14:42:51 +0200

    Mise a jourss

--- Prosfera_Setup/settings.py
+++ Prosfera_Setup/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path, os
-#from decouple import config
+from decouple import config
 import dj_database_url
 #from decouple import Config, Csv
 #config = Config()
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "SECRET_KEY =django-insecure-%-amfj!8!fy2qz*amz$)=f-lw8p4#kgm9ej-^!umuz=dgos!8!"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,16 +79,16 @@ WSGI_APPLICATION = 'Prosfera_Setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default' : dj_database_url.parse(Config('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default' : dj_database_url.parse(config('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
