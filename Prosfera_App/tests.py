3c05cb23eb77f05b7001e5a6b42f1b3dca132175
louiskizos <louisnyembo2@gmail.com>
2025-07-16 16:15:50 +0200

    commit pour le deployement sur render

--- Prosfera_App/tests.py
+++ Prosfera_App/tests.py
@@ -7,7 +7,7 @@ from django.test import TestCase
 # Initialisez Git dans votre projet : git init
 # Ajoutez les fichiers à Git : git add .
 # Faites un commit initial : git commit -m "Premier commit"
-# Ajoutez le dépôt distant : git remote add origin https://github.com/louiskizos/Prosfora.git
+# Ajoutez le dépôt distant : git remote add origin L
 # Poussez votre projet vers GitHub : git push -u origin main
 
 ######## git branch -m master main 
--- Prosfera_Setup/settings.py
+++ Prosfera_Setup/settings.py
@@ -12,8 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-#from decouple import config
-#import dj_database_url
+
+from decouple import config
+
+import dj_database_url
 #from decouple import Config, Csv
 #config = Config()
 
@@ -25,13 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%-amfj!8!fy2qz*amz$)=f-lw8p4#kgm9ej-^!umuz=dgos!8!"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 #ALLOWED_HOSTS = ["presfora.onrender.com", "localhost"]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -80,16 +82,18 @@ WSGI_APPLICATION = 'Prosfera_Setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default' : dj_database_url.parse(Config('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+#     'default' : dj_database_url.config(
+#         default=config('DATABASE_URL'),)
+    'default': dj_database_url.parse(config('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
