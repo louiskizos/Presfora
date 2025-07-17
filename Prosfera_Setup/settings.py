32c467102944e02be616b53322d02e3c8686e850
louiskizos <louisnyembo2@gmail.com>
2025-07-17 15:37:10 +0200

    deployement sur render avec super base

--- Prosfera_Setup/settings.py
+++ Prosfera_Setup/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 #ALLOWED_HOSTS = ["presfora.onrender.com", "localhost"]
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['presfora.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
